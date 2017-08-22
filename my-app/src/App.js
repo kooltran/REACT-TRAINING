594d8cc94c7b75496ddc261807ef6f79379d8eae
kooltran <kiettrankm11@gmail.com>
2017-08-22 23:14:42 +0700

    add stateless and stateful compenent

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,88 +5,48 @@ class App extends Component {
   render() {
     return (
       <div>
-        <PasswordInput />
-        <UserLists />
+        <CardList />
       </div>
     );
   }
 }
 
-class PasswordInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isShowPassword: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
 
-  handleClick() {
-    this.setState({
-      isShowPassword: !this.state.isShowPassword,
-    });
-  }
-
+// Stateless Component
+class CardItem extends Component {
   render() {
-    const { isShowPassword } = this.state.isShowPassword;
-    return (
-      <div>
-        <input type={isShowPassword ? 'text' : 'password'} />
-        <button onClick={this.handleClick}>{isShowPassword ? 'hide' : 'show'}</button>
-      </div>
-    );
-  }
-}
-
-class UserItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state  = {
-      isLiked: false,
-    }
-    this.getLike = this.getLike.bind(this);
-  }
-
-  getLike() {
-    this.setState({
-      isLiked: !this.state.isLiked,
-    })
-  }
-
-  render() {
-    const liked = this.state.isLiked;
     return (
       <div className="text-center user-item">
         <h3>{this.props.name}</h3>
         <img src={this.props.url} alt=''/>
         <p>{this.props.email}</p>
-        <button className={liked ? 'dislike': 'like'} onClick={this.getLike}>{liked ? 'dislike': 'like'}</button>
+        <button onClick={this.props.toggleLike}>{this.props.liked ? 'dislike': 'like'}</button>
       </div>
     )
   }
 }
 
-// const UserItem = (props) => {
-//   return (
-//     <li>
-//       <h3>{props.name}</h3>
-//       <img src={props.url} alt=''/>
-//       <p>{props.email}</p>
-//     </li>
-//   )
-// }
 
-class UserLists extends Component {
+//Stateful Component
+class CardList extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isLiked: false,
       users: [],
     }
     this.getUsers = this.getUsers.bind(this);
+    this.toggleLike = this.toggleLike.bind(this);
   }
 
   componentWillMount() {
     this.getUsers();
+  }
+
+  toggleLike() {
+    this.setState({
+      isLiked: !this.state.isLiked,
+    })
   }
 
   getUsers() {
@@ -105,7 +65,15 @@ class UserLists extends Component {
     const user = this.state.users;
     return (
       <div className="container">
-        {user.map((item, index) =>  <UserItem key={index} name={item.name.first} url={item.picture.large} email={item.email}/>)}
+        {user.map((item, index) =>
+          <CardItem
+              key={index}
+              name={item.name.first}
+              toggleLike={this.toggleLike}
+              liked={this.state.isLiked} url={item.picture.large}
+              email={item.email}
+          />
+        )}
         <a className="text-center" href="#" onClick={this.getUsers}>load more...</a>
       </div>
     )
