c1d02b6af5792b7ef5d1bba5db781e21721d01ab
kooltran <kiettrankm11@gmail.com>
2017-09-06 23:03:13 +0700

    demo react router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import CardList from './component/CardList';
+// import { Root } from './component/Root';
+import { Home } from './component/Home'
+import { User } from './component/User'
+import {Router, Route} from 'react-router';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <CardList />
+        {/* <CardList /> */}
+        <Router>
+          <Route path={"user"} component={User}/>
+          <Route path={"home"} component={Home}/>
+        </Router>
       </div>
     );
   }
---
+++ my-app/src/component/User.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class User extends Component {
+  render() {
+    return (
+      <div>
+        <h3>The User Page</h3>
+        <p>User Id: </p>
+      </div>
+    )
+  }
+}
